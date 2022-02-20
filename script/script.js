7fac6f7df914e85f7dcf417e1d0ad934109c5648
Luiz Guilherme Barbieri <barbieri_lgb@hotmail.com>
2022-02-20 10:32:08 -0300

    Adicionado função para ao clicar no menu, seja fechado

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
 const menuHamburger = document.querySelector(".icon-hamburguer");
+const nav = document.querySelector("#nav");
+const menuOptions = document.querySelector(".menu");
 
 function toggleMenu(){
-    const nav = document.querySelector("#nav");
     nav.classList.toggle("active");
 }
 
+
 menuHamburger.addEventListener("click", toggleMenu);
+menuOptions.addEventListener("click", toggleMenu);
+
+
+
