fb0e4492270bb043c008610c889b8d052485dd37
Luiz Guilherme Barbieri <barbieri_lgb@hotmail.com>
2022-02-20 11:25:17 -0300

    adicionado comentários no script, melhorado area de click do menu

--- script/script.js
+++ script/script.js
@@ -1,14 +1,19 @@
+// import icon menu from html.
 const menuHamburger = document.querySelector(".icon-hamburguer");
+// import menu from html.
 const nav = document.querySelector("#nav");
+// import options menu from html.
 const menuOptions = document.querySelector(".menu");
 
+// function add or remove class on menu to show options menu. Use class on CSS.
 function toggleMenu(){
     nav.classList.toggle("active");
 }
 
-function closeMenu(evento){
+// function close options menu when click out or on menu.
+function closeMenu(event){
     if(nav.classList.contains("active")){
-        if(evento.path[0].classList != "icon-hamburguer"){
+        if(event.path[0].classList != "icon-hamburguer"){
             toggleMenu();
         }
     }
